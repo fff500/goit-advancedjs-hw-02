86886b6d3fb36b94501275bb23ec580d7612e6c1
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-11-18 14:13:58 +0200

    Task 01 was added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const elements = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+
+let intervalId;
+
+elements.startBtn.addEventListener('click', startColorChange);
+elements.stopBtn.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+  intervalId = setInterval(() => {
+    elements.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  elements.startBtn.setAttribute('disabled', true);
+}
+
+function stopColorChange() {
+  clearInterval(intervalId);
+  elements.startBtn.removeAttribute('disabled', false);
+}
