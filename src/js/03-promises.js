f10805427fddbcf134a52c3eb7cd5716b8001365
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2023-11-19 20:08:46 +0200

    Task 03 was added

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const formDataObj = {};
+
+  for (let [key, value] of formData.entries()) {
+    formDataObj[key] = value;
+  }
+
+  const { delay, step, amount } = formDataObj;
+
+  setTimeout(() => {
+    for (let i = 1; i <= amount; i++) {
+      createPromise(i, i * step)
+        .then(({ position, delay }) => {
+          iziToast.success({
+            color: 'green',
+            message: `✅ Fulfilled promise ${position} in ${delay}ms`,
+          });
+        })
+        .catch(({ position, delay }) => {
+          iziToast.error({
+            color: 'red',
+            message: `❌ Rejected promise ${position} in ${delay}ms`,
+          });
+        });
+    }
+  }, delay);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
